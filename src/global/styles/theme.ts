bbddda74fff524cac14b95c51493e46698bf666d
Hudson Duarte <hudsoneeto@outlook.com>
2021-06-07 13:17:56 -0300

    feat: criando os temas globais

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    background: '#ffffff',
+
+    text: '#000000',
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  }
+}
