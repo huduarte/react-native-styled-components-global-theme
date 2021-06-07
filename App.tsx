3c5226a1af45c4f2820e9e302f9f45fe7afeb8a6
Hudson Duarte <hudsoneeto@outlook.com>
2021-06-07 13:18:36 -0300

    feat: adicionando tema global e home a nossa aplicaçao

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
+import {ThemeProvider} from 'styled-components';
+import theme from './src/global/styles/theme';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
